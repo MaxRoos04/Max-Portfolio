7f35646ea0d179d61bd94ca772f828e63492b7bd
keke rosberg <maxroos931@gmail.com>
2023-03-05 00:32:16 +0200

    Finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CountUp from 'react-countup';
 import {useInView} from 'react-intersection-observer'
 import {motion} from 'framer-motion'
-import {fadein} from '../variants'
+import {fadeIn} from '../variants'
 
 const About = () => {
   const [ref, inView] = useInView({
@@ -13,10 +13,20 @@ const About = () => {
   
   <section className='section' id='about' ref={ref}>
     <div className='conatiner mx-auto'>
-      <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center'>
-        <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top' style={{marginRight: "10%"}}></div>
+      <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+        <motion.div 
+        variants={fadeIn('right', 0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top' style={{marginRight: "10%"}}></motion.div>
         
-        <div className='flex-1'>
+        <motion.div 
+        variants={fadeIn('left', 0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='flex-1'>
           <h2 className='h2 text-accent'>About me.</h2>
           <h3 className='h3 mb-4'>I'm a student in information and communication technologies at Prakticum, studying my third and last year.</h3>
           <p className='mb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor. Etiam ut mattis ipsum.</p>
@@ -60,7 +70,7 @@ const About = () => {
             My portfolio
           </a>
         </div>
-       </div>
+       </motion.div>
       </div>
     </div>
     </section>
