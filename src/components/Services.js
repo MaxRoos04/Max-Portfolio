c891cd3aba960ee15635574312f0af45e4954fe5
keke rosberg <maxroos931@gmail.com>
2023-03-05 01:47:24 +0200

    Finished services page!

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,25 +7,25 @@ const services = [
   {
     name: 'U1/UX Design',
     description: 
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor. Etiam ut mattis ipsum.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor.',
     link: 'Learn More', 
   },
   {
-    name: 'Development',
+    name: 'Frontend development',
     description: 
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor. Etiam ut mattis ipsum.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor.',
     link: 'Learn More', 
   },
   {
-    name: 'Digital Marketing',
+    name: 'Learning Backend development',
     description: 
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor. Etiam ut mattis ipsum.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor.',
     link: 'Learn More', 
   },
   {
-    name: 'Product Branding',
+    name: 'Building computers',
     description: 
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor. Etiam ut mattis ipsum.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id ligula ut sapien mattis porttitor.',
     link: 'Learn More', 
   },
 ]
@@ -35,30 +35,45 @@ const Services = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col lg:flex-row'>
 
-        <div className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-6'>
+        <motion.div 
+        variants={fadeIn('right', 0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='flex-1 lg:bg-services lg:bg-bottom  bg-no-repeat mix-blend-lighten mb-12 lg:mb-6' style={{marginRight:'10%'}}>
           <h2 className='h2 to-accent mb-6'>What I Do.</h2>
-          <h3 className='h3 max-w-[455px] mb-16'>Im mostly intrested in frontend development and UI/UX</h3>
+          <h3 className='h3 max-w-[455px] mb-16 '>Im mostly intrested in frontend development and UI/UX design.</h3>
            <button className='btn btn-sm'>See my work</button>
-        </div>
+        </motion.div>
 
-        <div className='flex-1'>
+        <motion.div 
+        variants={fadeIn('left', 0.5)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='flex-1'>
           <div>
             {services.map((service, index) =>{
               const {name, description, link} = service; 
               return (
               
-              <div key={index}>
+              <div className='border-b border-white/20 h-[146px] mb-[38px] flex'  key={index}>
                 <div className='max-w-[476px]'>
                   <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}</h4>
                   <p className='font-secondary leading-tight'>{description}</p>
                 </div>
-                <div>{link}</div>
+                <div className='flex flex-col flex-1 items-end'>
+                  <a href='#' className='btn w-9 h-9 mb-[42px] flex justify-center items-center'>
+                    <BsArrowUpRight/>
+                  </a>
+                  <a href='#' className='text-gradient text-sm'>{link}</a>
+                </div>
               </div>
               );
 
             })}
           </div>
-        </div>
+        </motion.div>
 
       </div>
     </div>
